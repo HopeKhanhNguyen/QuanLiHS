5776940745815f75a091e746bffd7ea8f5216dcc
NGUYEN HUU KHANH <kh09092007@gmail.com>
2023-11-26 19:25:27 +0700

    Hehe

--- testpython.py
+++ testpython.py
@@ -1,5 +1,9 @@
-import requests
-
 class Checked() :
     def __init__(self) -> None:
-        self
+        pass
+    def show(self) :
+        print("Đây là repo test git của Nguyễn Hữu Khánh")
+
+if __name__ == "__main__" :
+    Checked().show()
+    
